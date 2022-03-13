aa126e5cd0e6609a4a8d2777bd1de8926cd94f81
computergeek1100 <66804731+computergeek1100@users.noreply.github.com>
2022-03-12 22:40:14 -0500

    Add equipment detail url path

--- theatre/urls.py
+++ theatre/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.DashboardView.as_view(), name='dashboard'),
     path('employees/', views.EmployeeList.as_view(), name='employees'),
     path('employees/<pk>/', views.EmployeeDetail.as_view(), name='employee-detail'),
-    path('equipment/', views.EquipmentList.as_view(), name='equipment')
+    path('equipment/', views.EquipmentList.as_view(), name='equipment'),
+    path('equipment/<pk>/', views.EquipmentDetail.as_view(), name='equipment-detail'),
 ]
